859955d5a34765c657803adc1f64fe33f980e13a
Siavash Samiei <siavash2samiei@gmail.com>
2017-01-06 20:08:35 -0500

    Fixed tracking!

--- iCar.ino
+++ iCar.ino
@@ -33,7 +33,7 @@ int getLDR(int LDR) {
 }
 void traverse (int dir[4], int speed1, int speed2){
   analogWrite(E1, speed1); // Run in full speed
-  analogWrite(E2, speed1);
+  analogWrite(E2, speed2);
   digitalWrite(I1, dir[0]);
   digitalWrite(I2, dir[1]);
   digitalWrite(I3, dir[2]);
@@ -44,66 +44,92 @@ void turnRightSharp(int speed1){
     traverse (dir, speed1, speed1);
 }
 void turnLeftSharp(int speed1){
-    int dir[4] = {0,1,1,0};
+    int dir[4] = {0,1,1,0}; 
     traverse (dir, speed1, speed1);
 }
-void backwards(int speed1){
-    int dir[4] = {1,0,1,0};
-    traverse (dir, speed1, speed1);
-}
-void forwards(int speed1){
-    int dir [4] = {0,1,0,1};
-    traverse (dir, speed1, speed1);
-}
-void turn(int speed1, int speed2){
-    int dir [4] = {1,0,0,1};
+void backwards(int speed1, int speed2){
+    int dir[4] = {0,1,0,1};
     traverse (dir, speed1, speed2);
-
 }
-   
+void forwards(int speed1, int speed2){
+    int dir [4] = {1,0,1,0};
+    traverse (dir, speed1, speed2);
+}
+/*
+void turn(int speed1, int speed2){
+    int dir [4] = {1,0,1,0};
+    traverse (dir, speed1, speed2);
+}
+*/   
 void loop() {
-    int LDR1 = getLDR(L1);
-    int LDR2 = getLDR(L2);
-    int LDR3 = getLDR(L3);
+    int LDR1 = getLDR(L1);//16-25
+    int LDR2 = getLDR(L2);//0-10
+    int LDR3 = getLDR(L3);//30-45
     //Serial.println(LDR1);
-    Serial.println(LDR2);
-    if (LDR2 > (LDR1 + 5) && LDR2 > (LDR3 + 5)){
-      forwards(70);
+    Serial.print(LDR1);
+    Serial.print("     ");
+    Serial.print(LDR2);
+    Serial.print("     ");
+    Serial.println(LDR3);
+    //Left = white middle = black right = white : Forward
+    if (LDR1 > 13 && LDR2 < 20 && LDR3 > 13){
+      Serial.println("Forwards");  
+      forwards(70,75);
     }
+    //Left = white middle = white right = black : Right
+    else if ((LDR1 > 13 && LDR2 > 17 && LDR3 < 17) || (LDR1 > 13 && LDR2 < 17 && LDR3 < 17)){
+      forwards (50,10);
+    } 
+    //Left = black middle = white right = white : Left
+    else if ((LDR1 < 13 && LDR2 > 17 && LDR3 > 17) || (LDR1 < 13 && LDR2 < 17 && LDR3 > 17)){
+      forwards (10,50);
+    }
+    // || (
+    //Left = white middle = white right = white OR Left = black middle = black right = black: Sharp Right
+    else if ((LDR1 > 50 && LDR2 > 50 && LDR3 > 6 0) || (LDR1 < 17 && LDR2 < 17 && LDR3 < 20) ){
+      turnRightSharp(60);
+    }
+  
     //General
-    if (abs(LDR1 - LDR2) <= 10 &&  abs(LDR1 - LDR3) <= 10 && abs(LDR2 - LDR3) <= 10){
-      turnRightSharp(70);
-    }
-    // Forwards adjustments
+    //if (abs(LDR1 - LDR2) <= 10 &&  abs(LDR1 - LDR3) <= 10 && abs(LDR2 - LDR3) <= 10){
+    //Serial.println("Sharp right"); 
+      //turnRightSharp(70);
+    //}
+   
+    /*// Forwards adjustments
     //Right
-    else if (LDR3 < 20 && LDR1 > 20 && LDR3 > 20){
-      turn(30,50);
+    else if (LDR3 < 10 && LDR1 > 10 && LDR3 > 10){
+     // Serial.println("Gradual right"); 
+     forwards(30,50);
     }
     //Left
-    else if (LDR3 < 20 && LDR1 > 20 && LDR3 > 20){
-      turn(50,30);
+    else if (LDR3 < 10 && LDR1 > 10 && LDR3 > 10){
+//      Serial.println("Gradual left");
+      forwards(50,30);
     }  
     // 90 Degree turns, might work for 45.
-    //Right
-    else if (LDR2 <= (LDR1 - 15) && 15 < abs(LDR3 - LDR2)){
-      turn(70,50);
+    //Rightx
+    else if (LDR2 <= (LDR1 - 15) && 30 < abs(LDR3 - LDR2)){
+  //    Serial.println("Faster right");
+      forwards(70,50);
     }
     //Left
-    else if (LDR2 <= (LDR3 - 15) && 15 < abs(LDR1 - LDR2)){
-      turn(50,70);
-    }
+    else if (LDR2 <= (LDR3 - 15) && 30 < abs(LDR1 - LDR2)){
+    //  Serial.println("Faster left");
+      forwads(50,70);
+    }*/
     
     //Test directions
     //turn (left, right);
-    //turn (100, 0);
+    //forwards(70, 75);
     
-    //Sample Test 1 code.
-    /*forwards(70);
+    /*//Sample Test 1 code.
+    turn(70,75);
     delay(4000);
-    turnRight(70,50);
+    turn(70,10);
     delay(3000);
-    backwards(70);
+    backwards(70,75);
     delay(4000);
-    turnLeft(50,70);
+    turn(10,70);
     delay(3000);*/
 }
